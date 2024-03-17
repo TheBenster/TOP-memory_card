c74e0ff227f46faa8221756e7e1028c51dc498d7
TheBenster <105664089+TheBenster@users.noreply.github.com>
2024-03-17 13:06:16 -0400

    change async package

--- src/components/PokemonCards.jsx
+++ src/components/PokemonCards.jsx
@@ -1,5 +1,5 @@
 import { React, useState, useEffect } from "react";
-import axios from "axios";
+import axios from "redaxios";
 import PokemonCard from "./PokemonCard";
 import "../stylesheets/ani.css";
 import "../index.css";
